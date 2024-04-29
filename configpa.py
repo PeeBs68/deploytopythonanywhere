e7794de291b79b974320ef521fc239a9ff6175c5
Phelim Barry <phelimb@yahoo.com>
2024-04-29 20:58:26 +0100

    Updated config file

---
+++ configpa.py
@@ -0,0 +1,4 @@
+config = {"host" : "phelimbarry.mysql.pythonanywhere-services.com",
+          "user" : "phelimbarry",
+          "password" : "wsaamysqlpapassword123",
+          "database" : "car"}
