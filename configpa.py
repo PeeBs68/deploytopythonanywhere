a3b4421caaf61cac07b63e1a055ef8a76ea6bff1
Phelim Barry <phelimb@yahoo.com>
2024-04-29 23:21:14 +0100

    testing

--- configpa.py
+++ configpa.py
@@ -1,4 +1,4 @@
 config = {"host" : "phelimbarry.mysql.pythonanywhere-services.com",
           "user" : "phelimbarry",
           "password" : "wsaamysqlpapassword123",
-          "database" : "phelimbarry$default"}
+          "database" : "phelimbarry$carsrus"}
